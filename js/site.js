538fe4ef345d865fe716dc15983a90dc191c3849
RobNawrot <87241529+RobNawrot@users.noreply.github.com>
2025-07-24 14:33:08 -0400

    Cleaning Up
    Removed the old alternative solution to the problem and crafted a more clean and optimal solution instead.
    
    Corrected a few minor errors and updated LinkedIn link.

--- js/site.js
+++ js/site.js
@@ -1,116 +1,98 @@
 
-// Get the Fizz and Buzz values from the page (Controller Function).
+// Get values from the user. Need to get the Fizz value and the Buzz value (Controller Function).
 function getValues() {
 
-    // Get values from the page.
-    let fizz = document.getElementById("fizzNum").value;
-    let buzz = document.getElementById("buzzNum").value;
+    // Get user values from the page.
+    let fizzValue = document.getElementById("fizzNum").value;
+    let buzzValue = document.getElementById("buzzNum").value;
 
-    // Attempt to cast the string values into integer values.
-    fizz = parseInt(fizz);
-    buzz = parseInt(buzz);
+    // Parsing given values from strings into integers.
+    fizzValue = parseInt(fizzValue);
+    buzzValue = parseInt(buzzValue);
 
-    // Input given for Fizz Buzz values must be validated. 
-    if (Number.isInteger(fizz) && Number.isInteger(buzz) && 
-    fizz >= 1 && fizz <= 100 && buzz >= 1 && buzz <= 100) {
-        // Call fizzBuzz().
-        let numbers = fizzBuzz(fizz, buzz);
-        // Call displayNumbers().
-        displayData(numbers);
+    // Making sure the given values are integers. If not, the user is notified of the error.
+    if (Number.isInteger(fizzValue) && Number.isInteger(buzzValue)) {
+        // Call fizzBuzz.
+        let fbArray = fizzBuzz(fizzValue, buzzValue);    
+        // Call displayData and write values to the screen.
+        displayData(fbArray);
     }
     else {
-        alert("ERROR - You must enter integer values within the range of 1 - 100.");
+        alert("You must enter integers!");
     }
 
 }
 
-// Alter all numbers that are multiples of either the Fizz/Buzz values or both (Logic Function). 
-function fizzBuzz(fizz, buzz) {
+// Alter all numbers that are multiples of either the Fizz/Buzz values or both (Logic Function).
+function fizzBuzz(fizzValue, buzzValue) {
 
-    const numbers = [];
-    let index = 0;
-
-    /* Populating an empty array with numbers 1-100. Finding all multiples
-    of the Fizz/Buzz values in the array and changing their respective 
-    values to "Fizz", "Buzz" or "FizzBuzz". */
-    for (let num = 1; num <= 100; num++) {
-        numbers.push(num);
-
-        /* Detecting if a number is a multiple of any passed value. If it
-        is, change the array's element value to "Fizz", "Buzz" or "FizzBuzz" 
-        respectively. */
-        if (numbers[index] % fizz == 0 && numbers[index] % buzz != 0) {
-            numbers[index] = "Fizz";
-            index++;     
+    // Initialize an array to be returned.
+    let returnArray = [];
+    
+    // Loop across all numbers (1 to 100).
+    for (let index = 1; index <= 100; index++) {
+        
+        /* We need to check the current value using three steps.
+        Check to see if the value is divisible by both fizzValue and buzzValue.
+        Check to see if divisible by only the fizzValue.
+        Check to see if divisible by only the buzzValue. */
+        if (index % fizzValue == 0 && index % buzzValue == 0) {
+            returnArray.push("FizzBuzz");
         }
-        else if (numbers[index] % buzz == 0 && numbers[index] % fizz != 0) {
-            numbers[index] = "Buzz";
-            index++;
+        else if (index % fizzValue == 0) {
+            returnArray.push("Fizz");
         }
-        else if (numbers[index] % fizz == 0 && numbers[index] % buzz == 0) {
-            numbers[index] = "FizzBuzz";
-            index++;
+        else if (index % buzzValue == 0) {
+            returnArray.push("Buzz");
         }
         else {
-            index++;
+            returnArray.push(index);
         }
-
     }
 
-    return numbers;
+    return returnArray;
 
 }
 
 /* Display the numbers and mark all Fizz values in GREEN, Buzz values in BLUE and 
-FizzBuzz values in RED (Display Function).  */
-function displayData(numbers) {
+FizzBuzz values in RED (Display Function). */
+function displayData(fbArray) {
 
-    let classname = "";
-    let i = 0;
-    let count = 0;
+    // Selects the tbody element from the app page.
+    let tableBody = document.getElementById("results");
 
-    /* General string "templateRows" used for generating an HTML table on the app page. 
-    While the "header" and "footer" variables represent each row of the table and 
-    encapsulate the table data elements. */
-    let templateRows = "";
-    let header = `<tr class="d-flex"> `;
-    let footer = ` </tr>`;
-    
-    // Looping through the array of numbers.
-    for (i = 0; i < numbers.length; i++) {
-         
-        /* Modifying and adding a cell into the table. Concatenates a string representing
-        a table data element onto the general string. */
-        if (numbers[i] == "Fizz") {
-            let classname = "fizz";
-            header += `<td class="${classname} flex-fill">${numbers[i]}</td>`;
-        }
-        else if (numbers[i] == "Buzz") {
-            let classname = "buzz";
-            header += `<td class="${classname} flex-fill">${numbers[i]}</td>`;
-        }
-        else if (numbers[i] == "FizzBuzz") {
-            let classname = "fizzbuzz";
-            header += `<td class="${classname} flex-fill">${numbers[i]}</td>`;
-        }
-        else {
-            let classname = "regular";
-            header += `<td class="${classname} flex-fill">${numbers[i]}</td>`;
-        }
+    /* Selects the template element which contains the structure for 
+    adding rows of data items to the table. */
+    let templateRow = document.getElementById("fbTemplate");
 
-        count++;
+    // Clears the table initially.
+    tableBody.innerHTML = "";
 
-        // Ensures that each row in the table displays five columns of data.
-        if (count == 5) {
-            header += footer;
-            templateRows += header;
-            header = `<tr class="d-flex"> `;
-            count = 0;
-        }
+    /* Loop over the array and create a table row that consists of 
+    five table data items, then add the row to the table. */
+    for (let index = 0; index < fbArray.length; index += 5) {
+        let tableRow = document.importNode(templateRow.content, true);
+        
+        // Grab the td elements to put into the array.
+        let rowCols = tableRow.querySelectorAll("td");
+        
+        rowCols[0].textContent = fbArray[index];
+        rowCols[0].classList.add(fbArray[index]);
 
+        rowCols[1].textContent = fbArray[index + 1];
+        rowCols[1].classList.add(fbArray[index + 1]);
+
+        rowCols[2].textContent = fbArray[index + 2];
+        rowCols[2].classList.add(fbArray[index + 2]);
+
+        rowCols[3].textContent = fbArray[index + 3];
+        rowCols[3].classList.add(fbArray[index + 3]);
+
+        rowCols[4].textContent = fbArray[index + 4];
+        rowCols[4].classList.add(fbArray[index + 4]);
+
+        // Add each row to the table.
+        tableBody.appendChild(tableRow);
     }
-
-    // Inserts the concatenated string into the 'tbody' HTML element.
-    document.getElementById("results").innerHTML = templateRows;
 
 }
